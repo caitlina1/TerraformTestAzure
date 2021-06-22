6a60fb3d8e151a1c836a41d4767f39a28cb64fda
caitlina1 <86077420+caitlina1@users.noreply.github.com>
2021-06-22 14:50:13 -0400

    Update main.tf

--- terraformFile/main.tf
+++ terraformFile/main.tf
@@ -1,86 +1,35 @@
-terraform {
-  required_providers {
-    azurerm = {
-      # Specify what version of the provider we are going to utilise
-      source = "hashicorp/azurerm"
-      version = ">= 2.4.1"
-    }
-  }
-}
 provider "azurerm" {
-  features {
-      key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
+  features {}
 }
-data "azurerm_client_config" "current" {}
-# Create our Resource Group - Jonnychipz-RG
-resource "azurerm_resource_group" "rg" {
-  name     = "jonnychipz-app01"
-  location = "UK South"
+
+
+resource "azurerm_resource_group" "ca825e-rg4" {
+  name     = "ca825e-rg4"
+  location = "eastus"
 }
-# Create our Virtual Network - Jonnychipz-VNET
-resource "azurerm_virtual_network" "vnet" {
-  name                = "jonnychipzvnet"
+
+resource "azurerm_virtual_network" "ca825e-vn3" {
+  name                = "ca825e-vn3"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "${azurerm_resource_group.ca825e-rg4.location}"
+  resource_group_name = "${azurerm_resource_group.ca825e-rg4.name}"
 }
-# Create our Subnet to hold our VM - Virtual Machines
-resource "azurerm_subnet" "sn" {
-  name                 = "VM"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes       = ["10.0.1.0/24"]
+
+resource "azurerm_subnet" "subnet" {
+    name                 = "internal"
+    resource_group_name  = "${azurerm_resource_group.ca825e-rg4.name}"
+    virtual_network_name = "${azurerm_virtual_network.ca825e-vn3.name}"
+    address_prefixes       = ["10.0.2.0/24"]
 }
-# Create our Azure Storage Account - jonnychipzsa
-resource "azurerm_storage_account" "jonnychipzsa" {
-  name                     = "jonnychipzsa"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags = {
-    environment = "jonnychipzrox"
-  }
-}
-# Create our vNIC for our VM and assign it to our Virtual Machines Subnet
-resource "azurerm_network_interface" "vmnic" {
-  name                = "jonnychipzvm01nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  
+
+resource "azurerm_network_interface" "ca825e-nic" {
+  name                = "ca825e-nic1"
+  location            = "${azurerm_resource_group.ca825e-rg4.location}"
+  resource_group_name = "${azurerm_resource_group.ca825e-rg4.name}"
+
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.sn.id
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
     private_ip_address_allocation = "Dynamic"
-  }
-}
-# Create our Virtual Machine - Jonnychipz-VM01
-resource "azurerm_virtual_machine" "jonnychipzvm01" {
-  name                  = "jonnychipzvm01"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.vmnic.id]
-  vm_size               = "Standard_B2s"
-  storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter-Server-Core-smalldisk"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "jonnychipzvm01os"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name      = "jonnychipzvm01"
-    admin_username     = "jonnychipz"
-    admin_password     = "Password123$"
-  }
-  os_profile_windows_config {
   }
 }
