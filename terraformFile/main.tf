c8090f0fa4a6617ab0169a31fe3307672ad55219
caitlina1 <86077420+caitlina1@users.noreply.github.com>
2021-06-22 15:16:46 -0400

    Update main.tf

--- terraformFile/main.tf
+++ terraformFile/main.tf
@@ -3,29 +3,29 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "ca825e-rg4" {
-  name     = "ca825e-rg4"
+resource "azurerm_resource_group" "ca825e-rg2" {
+  name     = "ca825e-rg2"
   location = "eastus"
 }
 
 resource "azurerm_virtual_network" "ca825e-vn3" {
   name                = "ca825e-vn3"
   address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.ca825e-rg4.location}"
-  resource_group_name = "${azurerm_resource_group.ca825e-rg4.name}"
+  location            = "${azurerm_resource_group.ca825e-rg2.location}"
+  resource_group_name = "${azurerm_resource_group.ca825e-rg2.name}"
 }
 
 resource "azurerm_subnet" "subnet" {
     name                 = "internal"
-    resource_group_name  = "${azurerm_resource_group.ca825e-rg4.name}"
+    resource_group_name  = "${azurerm_resource_group.ca825e-rg2.name}"
     virtual_network_name = "${azurerm_virtual_network.ca825e-vn3.name}"
     address_prefixes       = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "ca825e-nic" {
   name                = "ca825e-nic1"
-  location            = "${azurerm_resource_group.ca825e-rg4.location}"
-  resource_group_name = "${azurerm_resource_group.ca825e-rg4.name}"
+  location            = "${azurerm_resource_group.ca825e-rg2.location}"
+  resource_group_name = "${azurerm_resource_group.ca825e-rg2.name}"
 
   ip_configuration {
     name                          = "internal"
